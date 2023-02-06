4d2137b1533c164d16d0ccb5caa000493ce65b67
David Samson <david.andrew.engineer@gmail.com>
2023-02-06 00:05:13 -0500

    added rust implementation to project

---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use rand_distr::{Normal, Distribution, num_traits::clamp};
+use std::{io::{self, Write}, thread::sleep, time::Duration};
+
+// screen for now will be drawn in a terminal window with ascii characters
+struct TTYScreen {
+    width: u32,
+    height: u32,
+    data: Vec<char>,
+}
+
+impl TTYScreen {
+    fn new(width: u32, height: u32) -> TTYScreen {
+        let mut data = Vec::new();
+        for _ in 0..width * height {
+            data.push(' ');
+        }
+        TTYScreen {
+            width: width,
+            height: height,
+            data: data,
+        }
+    }
+
+    fn draw_at(&mut self, x: u32, y: u32, c: char) {
+        self.data[(y * self.width + x) as usize] = c;
+    }
+
+    fn clear(&mut self) {
+        for i in 0..self.width * self.height {
+            self.data[i as usize] = ' ';
+        }
+    }
+
+    fn draw(&self) {
+        //first move the cursor to the top left
+        print!("\x1b[0;0H");
+
+        for y in 0..self.height {
+            for x in 0..self.width {
+                print!("{}", self.data[(y * self.width + x) as usize]);
+            }
+            if y < self.height - 1 {
+                println!("");
+            }
+        }
+        //flush the output
+        io::stdout().flush().unwrap();
+
+    }
+}
+
+
+fn main()
+{
+    const WIDTH: u32 = 80;
+    const HEIGHT: u32 = 24;
+    const SLEEP_DURATION: Duration = Duration::from_millis(10); 
+
+    let mut screen = TTYScreen::new(WIDTH, HEIGHT);
+    
+    //coordinates of X character
+    let mut x:f64 = WIDTH as f64 / 2.0;
+    let mut y:f64 = HEIGHT as f64 / 2.0;
+    println!("x: {}, y: {}", x, y);
+
+    //random number generator
+    let normal = Normal::new(0.0, 1.0).unwrap();
+
+    //game loop
+    loop {
+        //////// Clear the screen ////////
+        screen.clear();
+        
+
+        //////// World updates go here ////////
+
+        //move the x by a random amount
+        x += normal.sample(&mut rand::thread_rng());
+        y += normal.sample(&mut rand::thread_rng());
+        
+        //clamp x and y into screen bounds
+        x = clamp(x, 1.0, WIDTH as f64 - 2.0);
+        y = clamp(y, 1.0, HEIGHT as f64 - 2.0);
+        
+        //draw the x at the current position (converted to integer)
+        screen.draw_at(x as u32, y as u32, 'X');
+        
+
+        
+        //////// Border around the screen ////////        
+        for x in 0..screen.width {
+            screen.draw_at(x, 0, '0');
+            screen.draw_at(x, screen.height - 1, '0');
+        }
+        for y in 0..screen.height {
+            screen.draw_at(0, y, '0');
+            screen.draw_at(screen.width - 1, y, '0');
+        }
+
+
+
+        //////// draw the screen, and sleep for a bit ////////
+        screen.draw();
+        sleep(SLEEP_DURATION);
+    }
+        
+}
